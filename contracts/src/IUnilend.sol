1b1d78c75cfffba7b3e42eace807d8584fd218d0
Ahmet <ahmtguler@gmail.com>
2024-11-16 16:59:36 +0700

    events updated

--- contracts/src/IUnilend.sol
+++ contracts/src/IUnilend.sol
@@ -28,14 +28,14 @@ interface IUnilend {
   error LowLevelCallFailed();
   error TokenNotOwned();
 
-  event LendCreated(address lender, uint256 tokenId, uint256 price, uint256 time);
+  event LendCreated(address indexed lender, uint256 indexed tokenId, uint256 price, uint256 time);
   event BorrowCreated(
-    address lender,
-    address borrower,
-    uint256 tokenId,
+    address indexed lender,
+    address indexed borrower,
+    uint256 indexed tokenId,
     uint256 price,
     uint256 deadline
   );
-  event BorrowCollected(address borrower, uint256 tokenId, uint256 amount0, uint256 amount1);
-  event TokenClaimedBack(address lender, address borrower, uint256 tokenId);
+  event BorrowCollected(address indexed borrower, uint256 indexed tokenId, uint256 amount0, uint256 amount1);
+  event TokenClaimedBack(address indexed lender, address indexed borrower, uint256 indexed tokenId);
 }
