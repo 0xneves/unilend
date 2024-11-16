928dc175dfb3a223145ad0621aaf16508e44c728
0xneves <asguilift@hotmail.com>
2024-11-16 17:52:54 +0700

    feat: Natspec

--- contracts/src/IUnilend.sol
+++ contracts/src/IUnilend.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.25;
 
 interface IUnilend {
+  /// Structs
   struct Lend {
     address lender;
     uint256 tokenId;
@@ -19,6 +20,7 @@ interface IUnilend {
     bool isActive;
   }
 
+  /// Errors
   error AlreadyBorrowed();
   error BorrowNotActive();
   error BorrowNotExpired();
@@ -28,7 +30,9 @@ interface IUnilend {
   error LowLevelCallFailed();
   error TokenNotOwned();
 
+  /// Events
   event LendCreated(address indexed lender, uint256 indexed tokenId, uint256 price, uint256 time);
+  event LendRevoked(address indexed lender, uint256 indexed tokenId);
   event BorrowCreated(
     address indexed lender,
     address indexed borrower,
@@ -36,6 +40,80 @@ interface IUnilend {
     uint256 price,
     uint256 deadline
   );
-  event BorrowCollected(address indexed borrower, uint256 indexed tokenId, uint256 amount0, uint256 amount1);
+  event BorrowCollected(
+    address indexed borrower,
+    uint256 indexed tokenId,
+    uint256 amount0,
+    uint256 amount1
+  );
   event TokenClaimedBack(address indexed lender, address indexed borrower, uint256 indexed tokenId);
+
+  /**
+   * @dev Lend a Uniswap V3 position
+   *
+   * Requirements:
+   *
+   * - The caller must own the token ID
+   *
+   * Emits a {LendCreated} event
+   *
+   * @param tokenId The token ID of the Uniswap V3 position
+   * @param price The price of the lend
+   * @param duration The duration of the lend
+   */
+  function lend(uint256 tokenId, uint256 price, uint256 duration) external;
+
+  /**
+   * @dev Borrow a Uniswap V3 position
+   *
+   * Requirements:
+   *
+   * - The lend must be active
+   * - The caller must not be the lender
+   * - The price must match the lend price
+   *
+   * Emits a {BorrowCreated} event
+   *
+   * @param tokenId The token ID of the Uniswap V3 position
+   */
+  function borrow(uint256 tokenId) external payable;
+
+  /**
+   * @dev Cancel a lend
+   *
+   * Requirements:
+   *
+   * - The lend must be owned by the caller
+   * - The lend must not be borrowed
+   *
+   * @param tokenId The token ID of the Uniswap V3 position
+   */
+  function cancelLend(uint256 tokenId) external;
+
+  /**
+   * @dev Claim back a borrowed Uniswap V3 position
+   *
+   * Requirements:
+   *
+   * - The borrow must be active
+   * - The deadline must have passed
+   *
+   * Emits a {TokenClaimedBack} event
+   *
+   * @param tokenId The token ID of the Uniswap V3 position
+   */
+  function claimTokenBack(uint256 tokenId) external;
+
+  /**
+   * @dev Collect the fees from a borrowed Uniswap V3 position
+   *
+   * Requirements:
+   *
+   * - The borrow must be active
+   *
+   * Emits a {BorrowCollected} event
+   *
+   * @param tokenId The token ID of the Uniswap V3 position
+   */
+  function collect(uint256 tokenId) external;
 }
--- contracts/src/Unilend.sol
+++ contracts/src/Unilend.sol
@@ -5,24 +5,42 @@ import "./interfaces/IERC721Receiver.sol";
 import "./interfaces/INonfungiblePositionManager.sol";
 import "./IUnilend.sol";
 
+/**
+ * @title Unilend - Uniswap V3 borrow and lending platform
+ */
 contract Unilend is IERC721Receiver, IUnilend {
+  // The NFT contract that mints Uniswap positions
   INonfungiblePositionManager public immutable nonfungiblePositionManager;
 
+  // Token ID to lends mapping
   mapping(uint256 => Lend) private _lends;
+  // Token ID to borrows mapping
   mapping(uint256 => Borrow) private _borrows;
 
+  /// @param _nonfungiblePositionManager The address of the Uniswap V3 NFT contract
   constructor(address _nonfungiblePositionManager) {
     nonfungiblePositionManager = INonfungiblePositionManager(_nonfungiblePositionManager);
   }
 
+  /**
+   * @param tokenId The token ID of the Uniswap V3 position
+   * @return lend The lend object
+   */
   function lends(uint256 tokenId) external view returns (Lend memory) {
     return _lends[tokenId];
   }
 
+  /**
+   * @param tokenId The token ID of the Uniswap V3 position
+   * @return borrow The borrow object
+   */
   function borrows(uint256 tokenId) external view returns (Borrow memory) {
     return _borrows[tokenId];
   }
 
+  /**
+   * @dev See {IUnilend-lend}
+   */
   function lend(uint256 tokenId, uint256 price, uint256 duration) external {
     if (nonfungiblePositionManager.ownerOf(tokenId) != msg.sender) revert TokenNotOwned();
 
@@ -39,6 +57,9 @@ contract Unilend is IERC721Receiver, IUnilend {
     emit LendCreated(msg.sender, tokenId, price, duration);
   }
 
+  /**
+   * @dev See {IUnilend-borrow}
+   */
   function borrow(uint256 tokenId) external payable {
     Lend memory _lend = _lends[tokenId];
 
@@ -65,17 +86,25 @@ contract Unilend is IERC721Receiver, IUnilend {
     emit BorrowCreated(_lend.lender, msg.sender, _lend.tokenId, _lend.price, _lend.time);
   }
 
+  /**
+   * @dev See {IUnilend-cancelLend}
+   */
   function cancelLend(uint256 tokenId) external {
     Lend memory _lend = _lends[tokenId];
 
     if (_borrows[tokenId].isActive) revert AlreadyBorrowed();
     if (_lend.lender != msg.sender) revert TokenNotOwned();
 
+    delete _lends[tokenId];
+
     nonfungiblePositionManager.safeTransferFrom(address(this), msg.sender, _lend.tokenId);
 
-    delete _lends[tokenId];
+    emit LendRevoked(_lend.lender, _lend.tokenId);
   }
 
+  /**
+   * @dev See {IUnilend-claimTokenBack}
+   */
   function claimTokenBack(uint256 tokenId) external {
     Borrow memory _borrow = _borrows[tokenId];
 
@@ -91,12 +120,18 @@ contract Unilend is IERC721Receiver, IUnilend {
     emit TokenClaimedBack(_borrow.lender, _borrow.borrower, _borrow.tokenId);
   }
 
+  /**
+   * @dev See {IUnilend-collect}
+   */
   function collect(uint256 tokenId) external {
     Borrow memory _borrow = _borrows[tokenId];
     if (!_borrow.isActive) revert BorrowNotActive();
     _collect(tokenId, _borrow.borrower);
   }
 
+  /**
+   * @dev Collect the fees from a borrowed Uniswap V3 position
+   */
   function _collect(uint256 tokenId, address recipient) internal {
     (uint256 amount0, uint256 amount1) = nonfungiblePositionManager.collect(
       INonfungiblePositionManager.CollectParams({
@@ -106,9 +141,13 @@ contract Unilend is IERC721Receiver, IUnilend {
         amount1Max: type(uint128).max
       })
     );
+
     emit BorrowCollected(recipient, tokenId, amount0, amount1);
   }
 
+  /**
+   * @dev Receive ERC721 token
+   */
   function onERC721Received(
     address,
     address,
